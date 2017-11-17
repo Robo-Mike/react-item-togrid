808ce15a42f0ce6dfe4b7d21eb6e63054dc5287c
Robo-Mike <mike.rawlinson@yahoo.com>
2017-11-17 13:27:57 +0000

    Switched to use react components and styling

--- src/components/ProductEdit.js
+++ src/components/ProductEdit.js
@@ -1,28 +1,33 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+//Form controls are just extensions of input type (though select is a bit different)
+import { Form , FormGroup, Button ,Col,FormControl, ControlLabel} from 'react-bootstrap';
 
 const ProductEdit = ({product, onProductTypeChange, productTypes}) => (
-<div>
-  <div className="form-group">
-    <label htmlFor="productDescription">Description ({product.description}) </label>
-    <input type='text'   id='productDescription' value={product.description} />
-  </div>
-  <div className="form-group">
-    <label htmlFor="productCost">Cost</label>
-    <input type='text'  value={product.cost}  id='productCost'/>
-  </div>
-  <div className="form-group">
-    <label htmlFor="productType">Type</label>
-    <select  value={product.type}  id='productType' onChange={(e) => onProductTypeChange(e)}  >
-      {
-         // each array element must include a key element for performance
-         productTypes.map( (type) => (
-           <option key={type.value} value={type.value} readOnly >{type.name}</option>
-         ))
-       }
-    </select>
-  </div>
-</div>
+
+  <Form horizontal>
+    <FormGroup >
+      <Col sm={6} componentClass={ControlLabel} >Description</Col>
+      <Col sm={3}><FormControl type='text'  value={product.description} /></Col>
+    </FormGroup>
+    <FormGroup>
+      <Col sm={6} componentClass={ControlLabel} >Cost</Col>
+      <Col sm={3}><FormControl type='text'   value={product.cost} /></Col>
+    </FormGroup>
+   <FormGroup>
+     <Col sm={6} componentClass={ControlLabel} >Item Type</Col>
+     <Col sm={3}>
+       <FormControl componentClass='select'   defaultValue={product.type} onChange={(e) => onProductTypeChange(e)} >
+        {
+           // each array element must include a key element for performance
+           productTypes.map( (type) => (
+             <option key={type.value} value={type.value} readOnly >{type.name}</option>
+           ))
+         }
+       </FormControl>
+    </Col>
+   </FormGroup>
+  </Form>
 )
 
 {/* ProductEdit.propTypes = {
